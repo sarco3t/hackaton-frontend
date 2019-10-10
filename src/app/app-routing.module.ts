9fff887b99e85e025da548123ec56925b3a0f437
Alex <alex@leverage.it>
2019-10-10 21:32:05 +0300

    rate component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from "./pages/home/home.component";
 import { LoginComponent } from "./pages/login/login.component";
 import { AboutComponent } from "./pages/about/about.component";
 import { SupportComponent } from "./pages/support/support.component";
+import { RateComponent } from "./pages/rate/rate.component";
 
 const routes: Routes = [
   {
@@ -22,6 +23,11 @@ const routes: Routes = [
   {
     path: "support",
     component: SupportComponent
+  }
+  ,
+  {
+    path: "rate",
+    component: RateComponent
   }
 ];
 
